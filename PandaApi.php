b51ea4eec66e2f8545a18758d9f8bb1795f3a6da
Christian Flothmann <christian.flothmann@xabbuh.de>
2013-01-12 18:25:20 +0100

    add api call for fetching encodings

--- PandaApi.php
+++ PandaApi.php
@@ -95,6 +95,85 @@ class PandaApi
     }
 
     /**
+     * Receive encodings from the server.
+     *
+     * Filters can be any set of key-value-pairs:
+     * <ul>
+     *   <li>status: one of 'success', 'fail' or 'processing' to filter by status</li>
+     *   <li>profile_id: filter encodings by profile id</li>
+     *   <li>profile_name: filter encodings by profile names</li>
+     *   <li>video_id: filter by video id
+     * </ul>
+     *
+     * @param array $filter Optional set of filters
+     * @return string The server response
+     */
+    public function getEncodings(array $filter = null)
+    {
+        return $this->restClient->get("/encodings.json", $filter);
+    }
+
+    /**
+     * Receive encodings filtered by status from the server.
+     *
+     * @see PandaApi::getEncodings()
+     * @param $status Status to filter by (one of 'success', 'fail' or 'processing')
+     * @param array $filter Additional optional filters (see
+     *     {@link PandaApi::getEncodings() PandaApi::getEncodings()} for a description
+     *     of the filters which can be used)
+     * @return string The server response
+     */
+    public function getEncodingsWithStatus($status, array $filter = null)
+    {
+        return $this->getEncodings(array("status" => $status), $filter);
+    }
+
+    /**
+     * Receive encodings filtered by a profile id from the server.
+     *
+     * @see PandaApi::getEncodings()
+     * @param $profileId Id of the profile to filter by
+     * @param array $filter Additional optional filters (see
+     *     {@link PandaApi::getEncodings() PandaApi::getEncodings()} for a description
+     *     of the filters which can be used)
+     * @return string The server response
+     */
+    public function getEncodingsForProfile($profileId, array $filter = null)
+    {
+        return $this->getEncodings(array("profile_id" => $profileId), $filter);
+    }
+
+    /**
+     * Receive encodings filtered by profile name from the server.
+     *
+     * @see PandaApi::getEncodings()
+     * @param $profileName Name of the profile to filter by
+     * @param array $filter Additional optional filters (see
+     *     {@link PandaApi::getEncodings() PandaApi::getEncodings()} for a description
+     *     of the filters which can be used)
+     * @return string The server response
+     */
+    public function getEncodingsForProfileByName($profileName, array $filter = null)
+    {
+        return $this->getEncodings(array("profile_name" => $profileName), $filter);
+    }
+
+    /**
+     * Receive encodings filtered by video from the server.
+     *
+     * @see PandaApi::getEncodings()
+     * @param $videoId Id of the video to filter by
+     * @param array $filter Additional optional filters (see
+     *     {@link PandaApi::getEncodings() PandaApi::getEncodings()} for a description
+     *     of the filters which can be used)
+     * @return string The server response
+     */
+    public function getEncodingsForVideo($videoId, array $filter = null)
+    {
+        return $this->getEncodings(array("video_id" => $videoId), $filter);
+    }
+
+    /**
      * Retrieve a cloud's details
      *
      * @param $cloudId The id of the cloud being fetched
