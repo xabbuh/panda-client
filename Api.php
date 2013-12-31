9c0feb33d66aeedf1c83ec2995e50415030c1526
Christian Flothmann <christian.flothmann@xabbuh.de>
2013-12-31 18:53:18 +0100

    adding a facade to ease API access

---
+++ Api.php
@@ -0,0 +1,236 @@
+<?php
+
+/*
+ * This file is part of the XabbuhPandaClient package.
+ *
+ * (c) Christian Flothmann <christian.flothmann@xabbuh.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Xabbuh\PandaClient;
+
+use Xabbuh\PandaClient\Api\Account;
+use Xabbuh\PandaClient\Api\AccountManager;
+use Xabbuh\PandaClient\Api\Cloud;
+use Xabbuh\PandaClient\Api\CloudManager;
+use Xabbuh\PandaClient\Api\RestClient;
+use Xabbuh\PandaClient\Transformer\CloudTransformer;
+use Xabbuh\PandaClient\Transformer\EncodingTransformer;
+use Xabbuh\PandaClient\Transformer\NotificationsTransformer;
+use Xabbuh\PandaClient\Transformer\ProfileTransformer;
+use Xabbuh\PandaClient\Transformer\TransformerFactory;
+use Xabbuh\PandaClient\Transformer\VideoTransformer;
+
+/**
+ * Simple entry point to the Panda API client.
+ *
+ * Basically, you receive an API instance by passing your cloud configuration
+ * to the static getInstance() method. Your configuration should look like
+ * this:
+ *
+ * - accounts:
+ *   - an identifier
+ *     - access_key: your access key
+ *     - secret_key: your secret key
+ *     - api_host: the API host to use
+ * - clouds:
+ *   - an identifier
+ *     - id: your cloud id
+ *     - account: one of your account identifiers as configured above
+ *
+ * A sample config can look like this:
+ *
+ * <code>
+ * $config = array(
+ *     'accounts' => array(
+ *         'default' => array(
+ *             'access_key' => ...,
+ *             'secret_key' => ...,
+ *             'api_host'   => 'api.pandastream.com',
+ *         ),
+ *     ),
+ *     'clouds' => array(
+ *         'default' => array(
+ *             'id'      => ...,
+ *             'account' => 'default',
+ *         ),
+ *     ),
+ * );
+ * </code>
+ *
+ * @author Christian Flothmann <christian.flothmann@xabbuh.de>
+ */
+class Api
+{
+    /**
+     * @var TransformerFactory
+     */
+    private $transformerFactory;
+
+    /**
+     * @var AccountManager
+     */
+    private $accountManager;
+
+    /**
+     * @var CloudManager
+     */
+    private $cloudManager;
+
+    /**
+     * @param TransformerFactory $transformerFactory
+     *
+     * @return Api
+     */
+    public function setTransformerFactory(TransformerFactory $transformerFactory)
+    {
+        $this->transformerFactory = $transformerFactory;
+        return $this;
+    }
+
+    /**
+     * @return TransformerFactory
+     */
+    public function getTransformerFactory()
+    {
+        return $this->transformerFactory;
+    }
+
+    /**
+     * @param AccountManager $accountManager
+     *
+     * @return Api
+     */
+    public function setAccountManager(AccountManager $accountManager)
+    {
+        $this->accountManager = $accountManager;
+        return $this;
+    }
+
+    /**
+     * @return AccountManager
+     */
+    public function getAccountManager()
+    {
+        return $this->accountManager;
+    }
+
+    /**
+     * @param CloudManager $cloudManager
+     *
+     * @return Api
+     */
+    public function setCloudManager(CloudManager $cloudManager)
+    {
+        $this->cloudManager = $cloudManager;
+        return $this;
+    }
+
+    /**
+     * @return CloudManager
+     */
+    public function getCloudManager()
+    {
+        return $this->cloudManager;
+    }
+
+    /**
+     * @param array $config
+     *
+     * @return Api
+     *
+     * @throws \InvalidArgumentException if the given configuration is invalid
+     */
+    public static function getInstance(array $config)
+    {
+        // register model transformers
+        $transformerFactory = new TransformerFactory();
+        $transformerFactory->registerTransformer('Cloud', new CloudTransformer());
+        $transformerFactory->registerTransformer('Encoding', new EncodingTransformer());
+        $transformerFactory->registerTransformer('Notifications', new NotificationsTransformer());
+        $transformerFactory->registerTransformer('Profile', new ProfileTransformer());
+        $transformerFactory->registerTransformer('Video', new VideoTransformer());
+
+        // register the accounts
+        $accountManager = new AccountManager(
+            isset($config['default_account']) ? $config['default_account'] : 'default'
+        );
+
+        if (!isset($config['accounts']) || !is_array($config['accounts'])) {
+            throw new \InvalidArgumentException('No account configuration given.');
+        }
+
+        foreach ($config['accounts'] as $name => $cloudConfig) {
+            foreach (array('access_key', 'secret_key', 'api_host') as $option) {
+                if (!isset($cloudConfig[$option])) {
+                    throw new \InvalidArgumentException(
+                        sprintf('Missing option %s for account %s', $option, $name)
+                    );
+                }
+            }
+
+            $accountManager->registerAccount(
+                $name,
+                new Account(
+                    $cloudConfig['access_key'],
+                    $cloudConfig['secret_key'],
+                    $cloudConfig['api_host']
+                )
+            );
+        }
+
+        // register the clouds
+        $cloudManager = new CloudManager(
+            isset($config['default_cloud']) ? $config['default_cloud'] : 'default'
+        );
+
+        if (!isset($config['clouds']) || !is_array($config['clouds'])) {
+            throw new \InvalidArgumentException('No cloud configuration given.');
+        }
+
+        foreach ($config['clouds'] as $name => $cloudConfig) {
+            foreach (array('id', 'account') as $option) {
+                if (!isset($cloudConfig[$option])) {
+                    throw new \InvalidArgumentException(
+                        sprintf('Missing option %s for cloud %s', $option, $name)
+                    );
+                }
+            }
+
+            try {
+                $account = $accountManager->getAccount($cloudConfig['account']);
+            } catch (\InvalidArgumentException $e) {
+                throw new \InvalidArgumentException(
+                    sprintf('Invalid account %s for cloud %s', $name, $cloudConfig['account'])
+                );
+            }
+
+            $restClient = new RestClient($cloudConfig['id'], $account);
+            $cloudManager->registerCloud(
+                $name,
+                new Cloud($restClient, $transformerFactory)
+            );
+        }
+
+        $api = new Api();
+        $api->setTransformerFactory($transformerFactory)
+            ->setAccountManager($accountManager)
+            ->setCloudManager($cloudManager)
+        ;
+
+        return $api;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return Cloud
+     */
+    public function getCloud($name)
+    {
+        return $this->cloudManager->getCloud($name);
+    }
+}
+ 
